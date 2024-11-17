fc323549ab09a067550a9ce41445c74449529efb
Sahil Patil <130278163+Sahil2k24@users.noreply.github.com>
2024-11-17 21:05:55 +0530

    Update while-part3.sh

--- WHILE/while-part3.sh
+++ WHILE/while-part3.sh
@@ -5,41 +5,43 @@ counter=1
 
 while [ $counter -lt 3 ]
 do
-	echo "Creating file$counter.txt"
-	touch file$counter.txt
-	sleep 1
-	echo "Created file$counter.txt"
-	echo
-	echo "Writing into file$counter.txt"
-	sleep 1
-	echo "hello I am $counter.
-	dsfkmaslkdfmlksamdflkmsldkmfsadfsa
-	df
-	sd
-	fsadf
-	sdf
-	
-	sdf
-	s
-	df
-	s
-	df
-	sd
-	f
-	s
-	df
-	sadf
-	asd
-	f
-	asdf
-	
-	sdf
-	sd
-	fsdf" > file$counter.txt
-	echo "Written into file$counter.txt"
-	echo
-	counter=$(($counter + 1))
-	echo "********************************************************"
+    echo "Creating file$counter.txt"
+    touch file$counter.txt
+    sleep 1
+    echo "Created file$counter.txt"
+    echo
+    echo "Writing into file$counter.txt"
+    sleep 1
+    cat <<EOF > file$counter.txt
+hello I am $counter.
+dsfkmaslkdfmlksamdflkmsldkmfsadfsa
+df
+sd
+fsadf
+sdf
+
+sdf
+s
+df
+s
+df
+sd
+f
+s
+df
+sadf
+asd
+f
+asdf
+
+sdf
+sd
+fsdf
+EOF
+    echo "Written into file$counter.txt"
+    echo
+    counter=$((counter + 1))
+    echo "********************************************************"
 done
 sleep 1
 echo "Out of the while block"
